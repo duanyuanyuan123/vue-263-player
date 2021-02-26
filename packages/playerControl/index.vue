a5a6ad1777bc7149a503d9a8a3c117372e25ba50
duanyuanyuan123 <2469241792@qq.com>
2021-02-26 17:44:42 +0800

    Add files via upload

---
+++ packages/playerControl/index.vue
@@ -0,0 +1,30 @@
+<template>
+   <div class="263_player_control">
+       <div @click="changeResolution" class="control_change_resolution">
+            <ul>
+                <li></li>
+            </ul>
+       </div>
+   </div>
+</template>
+<script>
+    export default {
+        name: 'playercontrol',
+        data () {
+           return {
+            resolutionList:[
+                {}
+            ]
+           }
+        },
+        methods: {
+            // 切换分辨率
+            changeResolution(){
+
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
